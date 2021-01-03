3ca48e263fd1cd2e656695f20c04fe111a004bda
Q-ora <76787490+Q-ora@users.noreply.github.com>
2021-01-03 16:35:10 +0800

    Create main.c

---
+++ 題目21. 最大值與最小值/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a[10], min, max;
+    int i;
+
+    for(i=0;i<10;i++)
+        scanf("%f",&a[i]);
+
+    min = a[0];
+    max = a[0];
+
+    for(i=1;i<10;i++)
+    {
+        if(a[i]>max) max = a[i];
+        if(a[i]<min) min = a[i];
+    }
+
+    printf("maximum:%.2f\n",max);
+    printf("minimum:%.2f\n",min);
+
+    return 0;
+}
